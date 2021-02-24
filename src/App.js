bc6c5176b7482f59e5760f548e9fd162e88599d1
russel <rasedulrussell@gmail.com>
2021-02-25 00:33:32 +0600

    contect component seperation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 import Contacts from './components/contacts';
 
+
     class App extends Component {
-      state = {
-        contacts: []
-      }
-      componentDidMount() {
-        fetch('http://localhost:8080/articles')
-        .then(res => res.json())
-        .then((data) => {
-          this.setState({ contacts: data })
-        })
-        .catch(console.log)
-      }
       render() {
         return (
-          <Contacts contacts={this.state.contacts} />
+          <Contacts />
         );
       }
     }
--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,19 +1,36 @@
 import React from 'react'
 
-const Contacts = ({ contacts }) => {
-  return (
-    <div>
-      <center><h1>Contact List</h1></center>
-      {contacts.map((contact) => (
-        <div class="card">
-          <div class="card-body">
-            <h6>{contact.title}</h6>
-            <p> {contact.details} </p>
+class Contacts extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: []
+    };
+  }
+  componentDidMount(){
+    fetch('http://localhost:8080/articles')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data })
+    })
+    .catch(console.log)
+  }
+  render() {
+    return (
+      <div>
+        <center><h1>News Details</h1></center>
+        {this.state.contacts.map((contact) => (
+          <div class="card">
+            <div class="card-body">
+              <h6>{contact.title}</h6>
+              <p> {contact.details} </p>
+              <a href="url">{contact.url}</a>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  )
-};
+        ))}
+      </div>
+    )
+  }
+}
 
 export default Contacts
